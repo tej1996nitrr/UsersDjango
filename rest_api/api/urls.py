c545db14769269d845a1691f4d8f5a5e07566754
Tejas Kanikdaley <tejaskanikdaley1996@gmail.com>
2020-04-01 18:54:25 +0530

    Update urls.py

--- rest_api/api/urls.py
+++ rest_api/api/urls.py
@@ -3,8 +3,6 @@ from .views import CreateCategoryView, CreatePostView, DetailsCategoryView, Deta
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('auth/', include('rest_framework.urls',
-                          namespace='rest_frame')),
     path('category/', CreateCategoryView.as_view(), name='create_category'),
     path('posts/', CreatePostView.as_view(), name='create_posts'),
     path('category/<int:pk>', DetailsCategoryView.as_view(), name='category_details'),
