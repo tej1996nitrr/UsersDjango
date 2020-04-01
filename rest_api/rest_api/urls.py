5c7a24ef63d37cf3a546bd8187cdf79dbcfb0011
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-04-01 12:10:28 +0530

    added screenshots

--- rest_api/rest_api/urls.py
+++ rest_api/rest_api/urls.py
@@ -18,4 +18,4 @@ urlpatterns = [
     path('schema/', schema_view),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, docuent_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
